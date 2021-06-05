47d19656b9ec845dd799f64910e65857fd22ac3a
Yohan Dhananjaya <yohan@incubatelabs.com>
2021-06-05 17:01:35 +0530

    updated the db

--- league_manager/settings.py
+++ league_manager/settings.py
@@ -76,29 +76,23 @@ WSGI_APPLICATION = 'league_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'league_manager',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        # 'TEST': {
-        #     'NAME': 'test_administrative',
-        # },
-        # 'OPTIONS': {
-        #     "charset": "utf8mb4"
-        # }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'league_manager',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
